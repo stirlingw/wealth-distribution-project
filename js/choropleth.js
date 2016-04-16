58bee48a93eedb7d0e443798ad1fad9d1b0796dd
swaite <swaite@rubiconproject.com>
2016-04-15 19:56:13 -0600

    adding data

--- js/choropleth.js
+++ js/choropleth.js
@@ -1,56 +1,161 @@
+var numberFormat = d3.format(".2f");
+
+var usChart = dc.geoChoroplethChart("#us-chart");
+var industryChart = dc.bubbleChart("#industry-chart");
+var roundChart = dc.bubbleChart("#round-chart");
+
+// Use the Queue.js library to read three files
+queue()
+	.defer(d3.csv, "data/vc.csv")
+    .defer(d3.json, "data/us-states.json")
+    .await(createVisualization);
 
 
-/*
- * Choropleth - Object constructor function
- * @param _parentElement 	-- the HTML element in which to draw the visualization
- * @param _data			    -- the
- */
+function createVisualization(error, dataCSV, statesJson){
+	var data = crossfilter(dataCSV);
 
-Choropleth = function(_parentElement, _data){
-	this.parentElement = _parentElement;
-	this.data = _data;
-	this.displayData = []; // see data wrangling
 
-	// DEBUG RAW DATA
-	console.log(this.data);
-	//this.initVis();
+
+//	var states = data.dimension(function (d) {
+//		return d["State"];
+//	});
+//	var stateRaisedSum = states.group().reduceSum(function (d) {
+//		return d["Raised"];
+//	});
+//
+//	var industries = data.dimension(function (d) {
+//		return d["Industry Group"];
+//	});
+//	var statsByIndustries = industries.group().reduce(
+//			function (p, v) {
+//				p.amountRaised += +v["Raised"];
+//				p.deals += +v["Deals"];
+//				return p;
+//			},
+//			function (p, v) {
+//				p.amountRaised -= +v["Raised"];
+//				if (p.amountRaised < 0.001) p.amountRaised = 0; // do some clean up
+//				p.deals -= +v["Deals"];
+//				return p;
+//			},
+//			function () {
+//				return {amountRaised: 0, deals: 0}
+//			}
+//	);
+//
+//	var rounds = data.dimension(function (d) {
+//		return d["RoundClassDescr"];
+//	});
+//	var statsByRounds = rounds.group().reduce(
+//			function (p, v) {
+//				p.amountRaised += +v["Raised"];
+//				p.deals += +v["Deals"];
+//				return p;
+//			},
+//			function (p, v) {
+//				p.amountRaised -= +v["Raised"];
+//				if (p.amountRaised < 0.001) p.amountRaised = 0; // do some clean up
+//				p.deals -= +v["Deals"];
+//				return p;
+//			},
+//			function () {
+//				return {amountRaised: 0, deals: 0}
+//			}
+//	);
+//
+//	usChart.width(990)
+//					.height(500)
+//					.dimension(states)
+//					.group(stateRaisedSum)
+//					.colors(d3.scale.quantize().range(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"]))
+//					.colorDomain([0, 200])
+//					.colorCalculator(function (d) { return d ? usChart.colors()(d) : '#ccc'; })
+//					.overlayGeoJson(statesJson.features, "state", function (d) {
+//						return d.properties.name;
+//					})
+//					.title(function (d) {
+//						return "State: " + d.key + "\nTotal Amount Raised: " + numberFormat(d.value ? d.value : 0) + "M";
+//					});
+//
+//	industryChart.width(990)
+//			.height(200)
+//			.margins({top: 10, right: 50, bottom: 30, left: 60})
+//			.dimension(industries)
+//			.group(statsByIndustries)
+//			.colors(d3.scale.category10())
+//			.keyAccessor(function (p) {
+//				return p.value.amountRaised;
+//			})
+//			.valueAccessor(function (p) {
+//				return p.value.deals;
+//			})
+//			.radiusValueAccessor(function (p) {
+//				return p.value.amountRaised;
+//			})
+//			.x(d3.scale.linear().domain([0, 5000]))
+//			.r(d3.scale.linear().domain([0, 4000]))
+//			.minRadiusWithLabel(15)
+//			.elasticY(true)
+//			.yAxisPadding(100)
+//			.elasticX(true)
+//			.xAxisPadding(200)
+//			.maxBubbleRelativeSize(0.07)
+//			.renderHorizontalGridLines(true)
+//			.renderVerticalGridLines(true)
+//			.renderLabel(true)
+//			.renderTitle(true)
+//			.title(function (p) {
+//				return p.key
+//						+ "\n"
+//						+ "Amount Raised: " + numberFormat(p.value.amountRaised) + "M\n"
+//						+ "Number of Deals: " + numberFormat(p.value.deals);
+//			});
+//	industryChart.yAxis().tickFormat(function (s) {
+//		return s + " deals";
+//	});
+//	industryChart.xAxis().tickFormat(function (s) {
+//		return s + "M";
+//	});
+//
+//	roundChart.width(990)
+//			.height(200)
+//			.margins({top: 10, right: 50, bottom: 30, left: 60})
+//			.dimension(rounds)
+//			.group(statsByRounds)
+//			.colors(d3.scale.category10())
+//			.keyAccessor(function (p) {
+//				return p.value.amountRaised;
+//			})
+//			.valueAccessor(function (p) {
+//				return p.value.deals;
+//			})
+//			.radiusValueAccessor(function (p) {
+//				return p.value.amountRaised;
+//			})
+//			.x(d3.scale.linear().domain([0, 5000]))
+//			.r(d3.scale.linear().domain([0, 9000]))
+//			.minRadiusWithLabel(15)
+//			.elasticY(true)
+//			.yAxisPadding(150)
+//			.elasticX(true)
+//			.xAxisPadding(300)
+//			.maxBubbleRelativeSize(0.07)
+//			.renderHorizontalGridLines(true)
+//			.renderVerticalGridLines(true)
+//			.renderLabel(true)
+//			.renderTitle(true)
+//			.title(function (p) {
+//				return p.key
+//						+ "\n"
+//						+ "Amount Raised: " + numberFormat(p.value.amountRaised) + "M\n"
+//						+ "Number of Deals: " + numberFormat(p.value.deals);
+//			});
+//	roundChart.yAxis().tickFormat(function (s) {
+//		return s + " deals";
+//	});
+//	roundChart.xAxis().tickFormat(function (s) {
+//		return s + "M";
+//	});
+//
+//	dc.renderAll();
 }
-
-
-Choropleth.prototype.initVis = function(){
-	var vis = this;
-
-	vis.margin = { top: 40, right: 0, bottom: 60, left: 60 };
-
-	vis.width = 800 - vis.margin.left - vis.margin.right,
-	vis.height = 400 - vis.margin.top - vis.margin.bottom;
-
-
-	// Update the visualization
-	//vis.wrangleData();
-}
-
-/*
- * Data wrangling
- */
-
-Choropleth.prototype.wrangleData = function(){
-	var vis = this;
-
-
-	//vis.updateVis();
-}
-
-
-
-/*
- * The drawing function - should use the D3 update sequence (enter, update, exit)
- * Function parameters only needed if different kinds of updates are needed
- */
-
-Choropleth.prototype.updateVis = function(){
-	var vis = this;
-
-}
-
-
