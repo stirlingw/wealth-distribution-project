3f8d8661325f13fbd41be04aaed5c5d855e55899
kgb007 <kirill00k@gmail.com>
2016-04-18 18:19:11 -0400

    Added files via upload
    updated barchart.js and main.js to make it work with the code

--- js/barchart.js
+++ js/barchart.js
@@ -1,86 +1,70 @@
+BarChart = function(_parentElement, _data){
+	this.parentElement = _parentElement;
+	this.data = _data;
+	this.displayData = []; // see data wrangling
 
+	// DEBUG RAW DATA
+	console.log(this.data);
+
+	this.initVis();
+}
+
+BarChart.prototype.initVis = function(){
+
+	var vis = this;
 // SVG drawing area
 
-var margin = {top: 40, right: 10, bottom: 60, left: 60};
+vis.margin = {top: 40, right: 10, bottom: 60, left: 60};
 
-var width = 900 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+vis.width = 900 - vis.margin.left - vis.margin.right,
+   vis.height = 600 - vis.margin.top - vis.margin.bottom;
 
-var svg = d3.select("#chart-area").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+vis.svg = d3.select("#bar-chart").append("svg")
+    .attr("width", vis.width + vis.margin.left + vis.margin.right)
+    .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
 
 // Our X scale
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
+vis.x = d3.scale.ordinal()
+    .rangeRoundBands([0, vis.width], .1);
 
 // Our Y scale
-var y = d3.scale.linear()
-    .rangeRound([height, 0]);
+vis.y = d3.scale.linear()
+    .rangeRound([vis.height, 0]);
 
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+vis.xAxis = d3.svg.axis()
+    .scale(vis.x)
+    .orient("bottom")
+.ticks(10);
 
-var yAxis = d3.svg.axis()
-    .scale(y)
+vis.yAxis = d3.svg.axis()
+    .scale(vis.y)
     .orient("left")
     .ticks(25, "s");
 
 
-var color = d3.scale.ordinal()
+vis.color = d3.scale.ordinal()
     .range(["yellow", "white", "#8a89a6", "#7b6888", "#6b486b", "red",  "green", "blue"]);
 
 
-// Initialize data
-loadData();
 
 
-// Load CSV file
-function loadData() {
-    d3.csv("data/berkeley-zuckman-average-real-wealth-per-family-data.csv", function(error, csv) {
+       vis.labelVar = 'year';
+        vis.varNames = d3.keys(vis.data[0])
+            .filter(function (key) { return key !== vis.labelVar;});
 
+        vis.color.domain(vis.varNames);
+        console.log(vis.varNames);
 
-        console.log(csv);
-
-        csv.forEach(function(d){
-           // d.year = +d.year;
-            d.bottom_90 = +d.bottom_90;
-            d.top_10 = +d.top_10;
-            d.top_05 = +d.top_05;
-            d.top_01 = +d.top_01;
-            d.top_005 = +d.top_005;
-            d.top_001 = +d.top_001;
-            d.top_0001 = +d.top_0001;
-
-
-        });
-
-
-
-        // Store csv data in global variable
-        data = csv;
-
-
-        var labelVar = 'year';
-        varNames = d3.keys(data[0])
-            .filter(function (key) { return key !== labelVar;});
-
-        color.domain(varNames);
-
-
-        console.log(varNames);
-
-        data.forEach(function (d) {
+        vis.data.forEach(function (d) {
             var y0 = 0;
-            d.mapping = varNames.map(function (name) {
+            d.mapping = vis.varNames.map(function (name) {
                 return {
                     name: name,
-                    label: d[labelVar],
+                    label: d[vis.labelVar],
                     y0: y0,
                     y1: y0 += +d[name]
                 };
@@ -88,69 +72,59 @@ function loadData() {
             d.total = d.mapping[d.mapping.length - 1].y1;
         });
 
-        data.sort(function(a, b) { return b.total - a.total; });
+        vis.data.sort(function(a, b) { return b.total - a.total; });
 
-        x.domain(data.map(function (d) { return d.year; }));
-        y.domain([0, d3.max(data, function (d) { return d.total; })]);
-
-        console.log("prepped data", data);
-
-        // Draw the visualization for the first time
-        updateVisualization();
-    });
-}
+        vis.x.domain(vis.data.map(function (d) { return d.year; }));
+       vis.y.domain([0, d3.max(vis.data, function (d) { return d.total; })]);
 
 
-// Render visualization
-function updateVisualization() {
-
-    var selection = svg.selectAll(".series")
-        .data(data)
+    vis.selection = vis.svg.selectAll(".series")
+        .data(vis.data)
         .enter().append("g")
         .attr("class", "series")
         .attr("transform", function (d) {
-            return "translate(" + x(d.year) + ",0)";
+            return "translate(" + vis.x(d.year) + ",0)";
         });
 
-    selection.selectAll("rect")
+    vis.selection.selectAll("rect")
         .data(function (d) { return d.mapping; })
         .enter().append("rect")
-        .attr("width", x.rangeBand())
-        .attr("y", function (d) { return y(d.y1); })
-        .attr("height", function (d) { return y(d.y0) - y(d.y1); })
-        .style("fill", function (d) { return color(d.name); })
+        .attr("width", vis.x.rangeBand())
+        .attr("y", function (d) { return vis.y(d.y1); })
+        .attr("height", function (d) { return vis.y(d.y0) - vis.y(d.y1); })
+        .style("fill", function (d) { return vis.color(d.name); })
         .style("stroke", "grey");
 
-    var legend = svg.selectAll(".legend")
-        .data(varNames.slice().reverse())
+    vis.legend = vis.svg.selectAll(".legend")
+        .data(vis.varNames.slice().reverse())
         .enter().append("g")
         .attr("class", "legend")
         .attr("transform", function (d, i) {
             return "translate(2," + i * 20 + ")";
         });
 
-    legend.append("rect")
-        .attr("x", width - 10)
+    vis.legend.append("rect")
+        .attr("x", vis.width - 10)
         .attr("width", 10)
         .attr("height", 10)
-        .style("fill", color)
+        .style("fill", vis.color)
         .style("stroke", "grey");
 
-    legend.append("text")
-        .attr("x", width - 12)
+    vis.legend.append("text")
+        .attr("x", vis.width - 12)
         .attr("y", 6)
         .attr("dy", ".35em")
         .style("text-anchor", "end")
         .text(function (d) { return d; });
 
-    svg.append("g")
+    vis.svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .attr("transform", "translate(0," + vis.height + ")")
+        .call(vis.xAxis);
 
-    svg.append("g")
+    vis.svg.append("g")
         .attr("class", "y axis")
-        .call(yAxis)
+        .call(vis.yAxis)
         .append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 2)
@@ -159,5 +133,26 @@ function updateVisualization() {
         .text("Wealth in Millions");
 
 
-};
 
+}
+
+/*
+ * Data wrangling
+ */
+BarChart.prototype.wrangleData = function(){
+	var vis = this;
+
+
+	// Update the visualization
+	vis.updateVis();
+}
+
+/*
+ * The drawing function - should use the D3 update sequence (enter, update, exit)
+ * Function parameters only needed if different kinds of updates are needed
+ */
+BarChart.prototype.updateVis = function(){
+	var vis = this;
+
+
+}
--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var allData = [];
 var household_income_dataset = [];
 var average_wealth_dataset = [];
+var average_real_wealth_per_family_dataset = [];
 
 // Date parser to convert strings to date objects
 var parseDate = d3.time.format("%Y").parse;
@@ -12,7 +13,7 @@ var parseYear = d3.time.format("%Y").parse;
 var colorScale = d3.scale.category10();
 
 // Variables for the visualization instances
-var choropleth, timeline, areaChart, lineChart, smallMultiples;
+var choropleth, timeline, areaChart, lineChart, smallMultiples, barChart;
 
 var formatCurrency = d3.format("$s");
 
@@ -23,10 +24,11 @@ queue()
 	.defer(d3.json, "data/us-states.json")
 	.defer(d3.csv,  "data/average_wealth.csv")
     .defer(d3.csv,  "data/census_median_household_income.csv")
+    .defer(d3.csv,  "data/berkeley-zuckman-average-real-wealth-per-family-data.csv")
 	.await(loadData);
 
 
-function loadData(error, dataCSV, statesJson, average_wealth_data, household_income_data){
+function loadData(error, dataCSV, statesJson, average_wealth_data, household_income_data, average_real_wealth_per_family_data){
 	if(!error){
         allData.data = crossfilter(dataCSV);
 
@@ -57,6 +59,26 @@ function loadData(error, dataCSV, statesJson, average_wealth_data, household_inc
 
         // Hand CSV data off to global var
         household_income_dataset = household_income_data;
+        
+        // ***********************************************************************
+        // Average real wealth per family
+        // ***********************************************************************
+        average_real_wealth_per_family_data.forEach(function (d) {
+            d.year = +d.year;
+            d.bottom_90 = +d.bottom_90;
+            d.top_10 = +d.top_10;
+            d.top_05 = +d.top_05;
+            d.top_01 = +d.top_01;
+            d.top_005 = +d.top_005;
+            d.top_001 = +d.top_001;
+            d.top_0001 = +d.top_0001;
+
+        });
+
+   
+        // Hand CSV data off to global var
+        average_real_wealth_per_family_dataset = average_real_wealth_per_family_data;
+        
 
         // ***********************************************************************
         // Create visualization
@@ -76,7 +98,7 @@ function createVis() {
     //	timeline = new Timeline("timeline", allData);
 	areaChart = new AreaChart("area-chart", average_wealth_dataset);
     smallMultiples = new SmallMultiples("small-multiples", household_income_dataset);
-
+    barChart = new BarChart("bar-chart", average_real_wealth_per_family_dataset);
 
 }
 
