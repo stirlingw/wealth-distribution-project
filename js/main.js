394f8faf7746ddf00148e5dd4bcd8aa0ae1c7afd
swaite <swaite@rubiconproject.com>
2016-04-19 00:01:48 -0600

    fixed UI/UX, and added new dataset

--- js/main.js
+++ js/main.js
@@ -118,35 +118,12 @@ function brushed() {
 }
 
 
-var current_classname, original_menu_item;
-
-window.$menu_items = $('.menu-item');
-
-window.$window = $(window);
-
-current_classname = '-current';
-
-original_menu_item = window.$menu_items.find("a[href='" + window.location.hash + "']").get(0);
-
-window.skrollr_instance = skrollr.init({
-  forceHeight: false,
-  render: $.throttle(100, function(skrollr_data) {
-    var $active_menu_items, $current_menu_item, href;
-    $active_menu_items = $menu_items.filter('.skrollable-between');
-    $current_menu_item = skrollr_data.curTop < $window.height() / 2 ? $menu_items.first() : $active_menu_items.length === 1 ? $active_menu_items : $active_menu_items.length > 1 ? $active_menu_items.eq(1) : $menu_items.last();
-    if ($current_menu_item.get(0) === $menu_items.get(0)) {
-      return typeof history !== "undefined" && history !== null ? typeof history.pushState === "function" ? history.pushState(null, null, window.location.pathname + window.location.search) : void 0 : void 0;
-    } else {
-      href = $current_menu_item.find('a').attr('href') || '#';
-      return typeof history !== "undefined" && history !== null ? typeof history.pushState === "function" ? history.pushState(null, null, href) : void 0 : void 0;
-    }
-  })
-});
-
-skrollr.menu.init(window.skrollr_instance);
-
-if (original_menu_item) {
-  $.delay(1000, function() {
-    return skrollr.menu.click(original_menu_item);
-  });
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
 }
+
+$(window).scroll(function(e){
+    parallax();
+});
