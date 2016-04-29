5af392117b994fc1620864a096f4916368e30063
swaite <swaite@rubiconproject.com>
2016-04-29 15:11:57 -0600

    fixing Choropleth

--- js/lineChart.js
+++ js/lineChart.js
@@ -65,7 +65,11 @@ LineChart.prototype.initVis = function(){
 		.text("Average Wealth ($)");
 
 	// Set ordinal color scale
-	vis.colorScale = d3.scale.category10();
+	//vis.colorScale = d3.scale.category10();
+	vis.colorScale = function(n) {
+      var colores_g = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
+      return colores_g[n % colores_g.length];
+    }
 
 	// Update color scale (all column headers except "Year")
 	// Color scale will be used later for the line chart
